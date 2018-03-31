c2878548ab4639c3bca2621ac4e8d7a093537586
Katie Wolfe <katie@dnaf.moe>
2018-03-31 05:34:40 -0400

    Document plugin.go

--- plugin.go
+++ plugin.go
@@ -1,7 +1,11 @@
 package vexlink
 
+// Plugin represents a Vexlink plugin that provides nodes.
 type Plugin struct {
-	Name    string
+	// Name is a unique identifier for this plugin. It should consist only of lowercase letters and numbers.
+	Name string
+	// Version is the build version of the plugin.
 	Version string
-	Nodes   map[string]Node
+	// Nodes is a map of node names to their prototype Node objects.
+	Nodes map[string]Node
 }
